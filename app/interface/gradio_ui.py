17495a5fc935c3f3f95a12f798be774b34edff3d
AlvaroMN1991 <alvaromorcillonuevo@hotmail.com>
2025-03-29 12:36:15 +0100

    Implementación de los diferentes modelos SAM (vit_b, vit_l, vit_h). Si el usuario no los tiene, se descagan del repositorio online y se pueden utuilizar en la aplicación

--- app/interface/gradio_ui.py
+++ app/interface/gradio_ui.py
@@ -1,13 +1,14 @@
 import gradio as gr
-import numpy as np
 import os
+from PIL import Image
 from app.lang.es import text_gradio_ui, text_general  # Importamos las cadenas de texto
-from app.utils.image_info import obtener_propiedades_imagen, Image
-from app.config import FORMATOS_TEXTO,FORMATOS_COMPATIBLES
+from app.utils.tools import obtener_propiedades_imagen, Image
+from app.utils.config import FORMATOS_TEXTO,FORMATOS_COMPATIBLES 
+from app.sam.sam_loader import cargar_sam_online, segmentar_automaticamente
 
 # Esta función toma una imagen cargada por el usuario y devuelve sus propiedades básicas
 def get_image_properties(filepath: str) -> str:
-    if not filepath:
+    if filepath is None or not os.path.exists(filepath):
         return text_gradio_ui["sin_imagen"]
   
     # Validar extensión del archivo
@@ -21,19 +22,72 @@ def get_image_properties(filepath: str) -> str:
         return text_gradio_ui["error_cargar_imagen"].format(error=e)
 
 
+#def lanzar_segmentacion(imagen: Image.Image, modelo_clave: str):
+def lanzar_segmentacion(filepath: str, modelo_clave: str):
+    if filepath is None or not os.path.exists(filepath):
+        return [], None
+
+    try:
+        imagen = Image.open(filepath)   
+    except Exception as e:
+        return None, None
+    
+    try:    
+        model = cargar_sam_online(modelo_clave)
+        imagenes_mascaras, combined_mask = segmentar_automaticamente(imagen, model)
+
+        return imagenes_mascaras, combined_mask
+    except Exception as e:
+        return None, None
+
+
 # Esta función crea la interfaz gráfica con Gradio
 def launch_interface():
     with gr.Blocks() as page:
         gr.Markdown(f"## {text_gradio_ui['titulo_app']}")
 
-        with gr.Row(scale=0):  # No se estira más de lo necesario)
-            # Cuado para subida de imagen
-            image_input = gr.Image(type="filepath", label=f"{text_gradio_ui['subir_imagen']} {FORMATOS_TEXTO}", height=512, width=512) 
-
-            # Cuadro con las propiedades de la imagen
-            image_info = gr.Textbox(label=text_gradio_ui["propiedades_imagen"], lines=4) 
+        with gr.Row(): 
+                with gr.Column():
+                # Cuadro para subida de imagen
+                    with gr.Tab("Imágen original"):
+                        image_input = gr.Image(type="filepath", label=f"{text_gradio_ui['subir_imagen']} {FORMATOS_TEXTO}") 
+                with gr.Column():
+                    with gr.Tab("Mascaras"):
+                        mascaras = gr.Gallery(label="🧩 Segmentos individuales", columns=4, rows=2)
+                    with gr.Tab("Mascaras Combinadas"):
+                        combined_mask_preview = gr.Image(label="🧵 Vista general de todas las segmentaciones", interactive=False)
+                    with gr.Tab("Imagen Final"):
+                        image_output = gr.Image(label="✅ Imagen sin HUD", interactive=False)               
+        
+        with gr.Row(): 
+            with gr.Column():
+                # Cuadro con las propiedades de la imagen
+                with gr.Tab(text_gradio_ui["propiedades_imagen"]):  
+                    image_info = gr.Textbox(label="", lines=4, interactive=False) 
+            with gr.Tab("Inpainting y Remover"):
+                with gr.Column():
+                    opciones_sam = [("SAM B (rápido, poca precisión)", "vit_b"), ("SAM L (Equilibrado)", "vit_l"), ("SAM H (El más preciso)", "vit_h")]
+                    sam_selector = gr.Radio(label="🧠 Modelo de SAM", choices=opciones_sam,  value="vit_b" ,interactive=True)
+                with gr.Column():
+                    opciones_inpainting = [("OpenCV", "1"), ("LaMa", "2"), ("Stable Diffusion", "3")]
+                    inpaint_selector = gr.Radio(label="🧠 Modelo de Inpainting", choices=opciones_inpainting, value="1", interactive=True)
+            with gr.Tab("Editor de imágen"):                
+                with gr.Column():
+                    opciones_inpainting = [("OpenCV", "1"), ("LaMa", "2"), ("Stable Diffusion", "3")]
+                    inpaint_selector = gr.Radio(label="🧠 Modelo de Inpainting", choices=opciones_inpainting, value="1", interactive=True)
+                with gr.Column():
+                    opciones_sam2 = [("SAM B (rápido, poca precisión)", "vit_b"), ("SAM L (Equilibrado)", "vit_l"), ("SAM H (El más preciso)", "vit_h")]
+                    sam_selector2 = gr.Radio(label="🧠 Modelo de SAM", choices=opciones_sam2, interactive=True)
+        with gr.Row():
+            segment_button = gr.Button("📐 Detectar Segmentos")
+            apply_button = gr.Button("🧹 Eliminar HUD")  
 
         # Cada vez que se suba una imagen, se llamará a esta función para analizarla
         image_input.change(fn=get_image_properties, inputs=image_input, outputs=image_info)
+        segment_button.click(fn=lanzar_segmentacion, inputs=[image_input, sam_selector], outputs=[mascaras, combined_mask_preview])
 
-    page.launch()
+
+    #descomentar para produccion
+    #page.launch()
+    #debug
+    page.launch(show_error=True, debug=True)
---
+++ app/utils/config.py
@@ -0,0 +1,10 @@
+# Formatos de imagen compatibles con Pillow
+FORMATOS_COMPATIBLES = (".jpg", ".jpeg", ".png", ".bmp", ".webp")
+FORMATOS_TEXTO = ", ".join(ext.upper().replace(".", "") for ext in FORMATOS_COMPATIBLES)
+MODELOS_SAM = {"vit_b": "facebook/sam-vit-base", "vit_l": "facebook/sam-vit-large", "vit_h": "facebook/sam-vit-huge"}
+# Diccionario de modelos disponibles
+MODEL_URLS = {
+    "vit_b": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth",
+    "vit_l": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth",
+    "vit_h": "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+}
